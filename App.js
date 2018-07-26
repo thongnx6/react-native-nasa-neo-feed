6e80a6495eedfd419e66a26d6401845ba16787cf
Thong Nguyen <thongnguyen@Thongs-MacBook-Pro-2.local>
2018-07-26 15:21:36 +0800

    Apply react-redux Provider and setup Store

--- App.js
+++ App.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu',
-});
+import configurationStore from './app/config/store';
+import Main from './app/screens/Main';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const store = configurationStore();
 
-export default () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>Welcome to React Native!</Text>
-    <Text style={styles.instructions}>To get started, edit App.js</Text>
-    <Text style={styles.instructions}>{instructions}</Text>
-  </View>
-);
+export default class App extends React.Component {
+  componentWillMount() {
+    // setup cache
+    // setup analytics
+    // setup local db
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Main />
+      </Provider>
+    );
+  }
+}
